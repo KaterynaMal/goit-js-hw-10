93307574b685028f5887e65c34b1f13552a1efc8
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-02 19:47:17 +0200

    +

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,17 +6,17 @@ axios.defaults.headers.common['x-api-key'] = apiKey;
 export function fetchBreeds() {
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data)
-    .catch(error => {
+    .then((response) => response.data)
+    .catch((error) => {
       throw error;
     });
 }
 
-function fetchCatByBreed(breedId) {
+export function fetchCatByBreed(breedId) {
   return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data)
-    .catch(error => {
+    .then((response) => response.data)
+    .catch((error) => {
       throw error;
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,64 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
-  selectCats: document.querySelector('.breed-select'),
+  breedSelect: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
+
+function hideLoader() {
+  refs.loader.style.display = 'none';
+  refs.breedSelect.disabled = false;
+}
+
+function showloader() {
+  refs.loader.style.display = 'block';
+  refs.breedSelect.disabled = true;
+  refs.catInfo.style.display = 'none';
+  refs.error.style.display = 'none';
+}
+
+function showError() {
+  refs.error.style.display = 'block';
+}
+
+fetchBreeds()
+    .then((breeds) => {
+        hideLoader();
+        breeds.forEach((breed) => {
+            const option = document.createElement("option");
+            option.value = breed.id;
+            option.textContent = breed.name;
+            breedSelect.appendChild(option);
+        });
+
+    })
+        .catch(() => {
+            hideLoader();
+            showError();
+        });
+
+refs.breedSelect.addEventListener("change", () => {
+    showloader();
+    const selectedBreedId = refs.breedSelect.value;
+
+    if (selectedBreedId) {
+        fetchCatByBreed(selectedBreedId)
+            .then((catData) => {
+                hideLoader;
+                const cat = catData[0];
+                refs.catInfo.innerHTML = `
+                 <img src="${cat.url}" alt="${cat.breeds[0].name}">
+          <h2>${cat.breeds[0].name}</h2>
+          <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
+          <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
+                `;
+                refs.catInfo.style.disabled = "block";
+            })
+            .catch(() => {
+                hideLoader();
+                showError();
+        })
+    }
+        })
